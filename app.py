28059468672a2e59d1274a39b431a3c235ca913e
SajeelKA <sajeelk7@gmail.com>
2025-01-21 18:09:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, request
-import numpy as np
-import pandas as pd
 
 app = Flask(__name__)
 
@@ -9,14 +7,6 @@ app = Flask(__name__)
 def hello():
     return render_template('myImage.html')
 
-
-
-def adding(n1, n2):
-    df = pd.DataFrame({"Name": ['sajeel'],
-                     
-                   "Address": ['abc'],
-                   })
-    return df.to_html(classes='table tablt-stripped')
     
    
 
